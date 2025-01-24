e441d71050654296d75040432d17e90a3326f5f1
souzafcharles <souzach@outlook.com>
2025-01-24 10:00:34 -0300

    Refactoring Episode 01 | README.md and import.sql | Java Spring Intensive

--- Episode_1_Structured_Project/gamelist/src/main/java/com/souza/charles/dslist/repositories/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package com.souza.charles.dslist.repositories;
-
-import com.souza.charles.dslist.entities.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
